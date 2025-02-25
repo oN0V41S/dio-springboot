4b82ff1601c7c61ac2171447ab96af65200dd598
Novais Rafael <rafaelaugustonnovais@gmail.com>
2025-02-25 20:40:43 +0000

    firts commit

---
+++ firts-steps/src/main/java/dio/firts_steps/FirtsStepsApplication.java
@@ -0,0 +1,13 @@
+package dio.firts_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirtsStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirtsStepsApplication.class, args);
+	}
+
+}
---
+++ firts-steps/src/test/java/dio/firts_steps/FirtsStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.firts_steps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirtsStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
